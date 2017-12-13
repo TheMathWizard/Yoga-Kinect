c73241e285067b552baa884545e8ee6caaf8991f
TheMathWizard <jroshannaik@gmail.com>
2017-12-13 21:05:50 +0530

    Delete keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-import keyboard
-'''k = keyboard.read_key()  # in my python interpreter, this captures "enter up"
-k = keyboard.read_key()'''
-recorded = keyboard.record(until='esc')
-# Then replay back at three times the speed.
-print(recorded)
